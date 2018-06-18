91c3be34f504ac97b14c49df6ef20097b316a063
Clyde D'Souza <clyd94@gmail.com>
2018-06-19 00:56:06 +1200

    intial commit

---
+++ Calculator.Web/Scripts/app.js
@@ -0,0 +1,8 @@
+﻿
+// Entry point of app
+$(document).ready(function () {
+    $("#sumOf2Numbers").html(addition(5, 7));
+    $("#subOf2Numbers").html(subtraction(20, 5));
+    $("#mulOf2Numbers").html(multiplication(13, 8));
+    $("#divOf2Numbers").html(division(16, 4));
+});
---
+++ Calculator.Web/Scripts/tests/app/appHelper.test.js
@@ -0,0 +1,35 @@
+﻿describe("addition", () => {
+    it("returns addition of two numbers", () => {
+        expect(addition(6, 7)).toBe(13);
+    });
+    it("returns addition of two numbers where one number is a negative number", () => {
+        expect(addition(6, -2)).toBe(4);
+    });
+});
+
+describe("subtraction", () => {
+    it("returns subtraction of two numbers", () => {
+        expect(subtraction(8, 2)).toBe(6);
+    });
+    it("returns subtraction of two numbers where one number is a negative number", () => {
+        expect(subtraction(6, -2)).toBe(8);
+    });
+});
+
+describe("multiplication", () => {
+    it("returns multiplication of two numbers", () => {
+        expect(multiplication(6, 2)).toBe(12);
+    });
+    it("returns multiplication of two numbers where one number is a negative number", () => {
+        expect(multiplication(6, -3)).toBe(-18);
+    });
+});
+
+describe("division", () => {
+    it("returns division of two numbers", () => {
+        expect(division(8, 2)).toBe(4);
+    });
+    it("returns division of two numbers where one number is a negative number", () => {
+        expect(division(8, -2)).toBe(-4);
+    });
+});
