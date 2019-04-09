3d93d1bffa446551b8414a49c53b89bb0d288dd6
Clyde D'Souza <clyd94@gmail.com>
2019-04-09 19:54:42 +1200

    Fixed jasmine chutzpah unit tests

--- Calculator.Web/Scripts/tests/app/appHelper.test.js
+++ Calculator.Web/Scripts/tests/app/appHelper.test.js
@@ -1,35 +1,37 @@
-﻿describe("addition", () => {
-    it("returns addition of two numbers", () => {
+﻿/// <reference path="../../apphelper.js" />
+
+describe("addition", function () {
+    it("returns addition of two numbers", function() {
         expect(addition(6, 7)).toBe(13);
     });
-    it("returns addition of two numbers where one number is a negative number", () => {
+    it("returns addition of two numbers where one number is a negative number", function ()  {
         expect(addition(6, -2)).toBe(4);
     });
 });
 
-describe("subtraction", () => {
-    it("returns subtraction of two numbers", () => {
+describe("subtraction", function () {
+    it("returns subtraction of two numbers", function ()  {
         expect(subtraction(8, 2)).toBe(6);
     });
-    it("returns subtraction of two numbers where one number is a negative number", () => {
+    it("returns subtraction of two numbers where one number is a negative number", function ()  {
         expect(subtraction(6, -2)).toBe(8);
     });
 });
 
-describe("multiplication", () => {
-    it("returns multiplication of two numbers", () => {
+describe("multiplication", function () {
+    it("returns multiplication of two numbers", function () {
         expect(multiplication(6, 2)).toBe(12);
     });
-    it("returns multiplication of two numbers where one number is a negative number", () => {
+    it("returns multiplication of two numbers where one number is a negative number", function () {
         expect(multiplication(6, -3)).toBe(-18);
     });
 });
 
-describe("division", () => {
-    it("returns division of two numbers", () => {
+describe("division", function () {
+    it("returns division of two numbers", function ()  {
         expect(division(8, 2)).toBe(4);
     });
-    it("returns division of two numbers where one number is a negative number", () => {
+    it("returns division of two numbers where one number is a negative number", function ()  {
         expect(division(8, -2)).toBe(-4);
     });
 });
